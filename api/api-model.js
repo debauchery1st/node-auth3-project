aeb66b5b41912f8a30b2d963b9ef9621327a5820
debauchery1st <trevor.j.martin@gmail.com>
2020-02-26 17:31:12 -0500

    api model

--- api/api-model.js
+++ api/api-model.js
@@ -1,1 +1,26 @@
-// todo
+const db = require(".../database/dbConfig");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
+
+function add(user) {
+  return db("users").insert(user);
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function find() {
+  return db("users").select("id", "username");
+}
--- api/api-router.js
+++ api/api-router.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
+const api = require("./api-model");
 
 router.get("/", (req, res) => {
   res.status(200).send("Authentication using JSON Web Tokens (JWTs)");
+});
+
+router.post("/register", (req, res) => {
+  req.body.username &&
+    req.body.password &&
+    api.add(req.body).then(newUser => {
+      console.log(newUser);
+    });
+  //
+});
+
+router.post("/login", (req, res) => {
+  // todo
 });
 
 module.exports = router;
